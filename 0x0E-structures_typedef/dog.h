eb7eef188956df96f62b2ac5d9438aeb7e18fe3d
I-Frostbyte <irefyre@gmail.com>
2023-03-20 11:00:05 -0700

    dog updated again

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,6 @@
 /**
 typedef struct dog dog_t;
 **/
-
 /**
  * struct dog - struct that stores some information of a dog
  * @name: name of the dog
@@ -23,10 +22,9 @@ struct dog
 	float age;
 	char *owner;
 };
-
+/**
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/**
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 **/
